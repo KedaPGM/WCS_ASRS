4e09f78ba14a76b67a4815e33c1e19939d65e45d
Dragon-DL <88134035+Dragon-DL@users.noreply.github.com>
2022-02-13 14:35:20 +0800

    修改度固定地址

--- WcsAsra/Model/DeviceBase.cs
+++ WcsAsra/Model/DeviceBase.cs
@@ -8,7 +8,7 @@ namespace WcsAsra.Model
 {
     public class DeviceBase : ViewModelBase
     {
-        public string Ip = "10.9.30.26";              //地址
+        public string Ip = "192.168.3.1";              //地址
         public int Port = 2002;                       //端口
 
         public Device device;
